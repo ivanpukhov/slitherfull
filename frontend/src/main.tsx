3fba7ea5150b2ed8f9af710d5fd710b29ff9ff95
ivaninbox <78599006+ivanpukhov@users.noreply.github.com>
2025-11-01 15:29:06 +0500

    Add SPA routing for admin dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { TermsOfService } from './pages/TermsOfService'
-import { PrivacyPolicy } from './pages/PrivacyPolicy'
 import './styles/main.css'
 import { ToastProvider } from './hooks/useToast'
-
-const router = createBrowserRouter([
-  { path: '/', element: <App /> },
-  { path: '/terms-of-service', element: <TermsOfService /> },
-  { path: '/terms-of-service.html', element: <TermsOfService /> },
-  { path: '/privacy-policy', element: <PrivacyPolicy /> },
-  { path: '/privacy-policy.html', element: <PrivacyPolicy /> }
-])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ToastProvider>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ToastProvider>
   </React.StrictMode>
 )
